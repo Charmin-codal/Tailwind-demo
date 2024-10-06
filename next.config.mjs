a9874ccf7132684f8d1c83296b18a29c9966a901
Charmin Patel <cpatel@codal.com>
2024-10-06 23:00:55 +0530

    add public access

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+module.exports = {
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' },
+        // Add more routes here if needed
+      }
+    },
+    // Set custom output directory
+    distDir: 'public',
+  }
+  
 export default nextConfig;
